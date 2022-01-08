6229fba2f33efc2c63bfa74682dce41adbf0e5b5
brettman <brettman@gmail.com>
2022-01-08 13:22:37 +0200

    interesting use of labels to break an outter loop;

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,21 @@
 fn main() {
-    let condition = true;
+    let mut count = 0;
 
-    // if is an *expression* so can be used as an assignment
-    let number = if condition { 5 } else {6};
-    println!("The value of the number is {}", number);
+    'counting_up: loop{
+
+        println!("count = {}", count);
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {}", remaining);
+            if remaining == 9 {
+                break;
+            }
+            if count == 2{
+                break 'counting_up
+            }
+            remaining -= 1;
+        }
+        count +=1;
+    }
 }
